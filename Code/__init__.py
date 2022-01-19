bbd8408f12468801719af1b45a11990d816b0dd2
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-19 10:03:39 +0530

    Update __init__.py

--- Code/__init__.py
+++ Code/__init__.py
@@ -1,1 +1,10 @@
+from pyrogram import Client,filters
+from .config import Config
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
