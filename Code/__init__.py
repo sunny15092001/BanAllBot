7ac1ba66bcec0427e31f7492ada3bd384b16beff
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-26 09:11:26 +0530

    Update __init__.py

--- Code/__init__.py
+++ Code/__init__.py
@@ -6,14 +6,18 @@ class Config:
     TELEGRAM_TOKEN=os.environ['TELEGRAM_TOKEN']
     TELEGRAM_APP_HASH=os.environ['TELEGRAM_APP_HASH']
     TELEGRAM_APP_ID=int(os.environ['TELEGRAM_APP_ID'])
+    TELEGRAM_SUDO_ID=int(os.environ['TELEGRAM_SUDO_ID'])
     if not TELEGRAM_TOKEN:
         raise ValueError('TELEGRAM BOT TOKEN not set')
     
     if not TELEGRAM_APP_HASH:
-        raise ValueError("TELEGRAM_APP_HASH not set")
+        raise ValueError("TELEGRAM_APP_HASH not set, set it first")
 
     if not TELEGRAM_APP_ID:
-        raise ValueError("TELEGRAM_APP_ID not set")
+        raise ValueError("TELEGRAM_APP_ID not set, set it first")
+
+    if not TELEGRAM_SUDO_ID:
+        raise ValueError("TELEGRAM_SUDO_ID not set, set it first")
 
 
 logging.basicConfig(
