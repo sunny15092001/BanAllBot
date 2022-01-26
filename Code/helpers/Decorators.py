e1409c37840691055e7fa285ac49b96c22819e89
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-26 09:13:04 +0530

    Update Decorators.py

--- Code/helpers/Decorators.py
+++ Code/helpers/Decorators.py
@@ -1,1 +1,14 @@
+from typing import Callable
 
+from pyrogram import Client
+from pyrogram.types import Message
+
+from Code import TELEGRAM_SUDO_ID as SUDO_USERS
+
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+    return decorator
